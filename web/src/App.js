6b0fc63fb8b474798ed58a47a624f68f69fe97cb
Coord <coordinater@gmail.com>
2018-06-30 15:52:13 -0700

    Add basket component

--- web/src/App.js
+++ web/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter,
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/home/Login';
+import Basket from './components/order/Basket';
 import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
@@ -13,7 +14,9 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path={'/'} component={() =><Login/>} />
+          <Route exact path={'/review'} component={() =><Basket/>} />
           <Route component={Login} />
+
         </Switch>
       </BrowserRouter>
     );
