d762dd6b25d35111f54bcefda9d65f8c40822fe7
yichiang <yi_chiang@Outlook.com>
2018-06-30 13:47:52 -0700

    set up router pages

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component } from 'react';
+import { BrowserRouter,
+         Route,
+         Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/home/Login';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={'/'} component={() =><Login/>} />
+          <Route component={Login} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
