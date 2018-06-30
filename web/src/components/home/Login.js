59057d81db04bb75253a02cf67be4bf3f5b602ff
yichiang <yi_chiang@Outlook.com>
2018-06-30 14:23:14 -0700

    add camera to login page

--- web/src/components/home/Login.js
+++ web/src/components/home/Login.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 import './../../App.css';
-import { Button, Checkbox, Form, Segment } from 'semantic-ui-react'
-import homeStyle from './../../css/home.css';
+import { Button, Checkbox, Form, Segment, Divider, Header } from 'semantic-ui-react'
+import './../../css/home.css';
+import Camera from 'react-camera';
+
+
+const style = {
+  preview: {
+    position: 'relative',
+  },
+  captureContainer: {
+    display: 'flex',
+    position: 'absolute',
+    justifyContent: 'center',
+    zIndex: 1,
+    bottom: 0,
+    width: '100%'
+  },
+  captureButton: {
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    height: 56,
+    width: 56,
+    color: '#000',
+    margin: 20
+  },
+  captureImage: {
+    width: '100%',
+  }
+};
 
 class Login extends Component {
+  constructor(props) {
+   super(props);
+   this.takePicture = this.takePicture.bind(this);
+ }
+
+ takePicture() {
+   this.camera.capture()
+   .then(blob => {
+     this.img.src = URL.createObjectURL(blob);
+     this.img.onload = () => { URL.revokeObjectURL(this.src); }
+   })
+ }
+
   render() {
     return (
       <div>
-        <div className={homeStyle.pl_login_form}> Hi </div>
-      <Segment raised className={homeStyle.pl_login_form}>
+
+      <Segment raised className={'pl_login_form'}>
         <Form>
         <Form.Field>
           <label>First Name</label>
@@ -20,8 +60,32 @@ class Login extends Component {
         </Form.Field>
         <Button type='submit'>Submit</Button>
         </Form>
+        <Divider horizontal>Or</Divider>
+        <div>
+          <Camera
+              style={style.preview}
+              ref={(cam) => {
+                this.camera = cam;
+              }}
+            >
+              <div style={style.captureContainer} onClick={this.takePicture}>
+                <div style={style.captureButton} />
+              </div>
+            </Camera>
 
+            <Header size='huge'>Is Yi?</Header>
+            <Button primary>Login</Button>
+
+        </div>
       </Segment>
+
+
+        {/* <img
+          style={style.captureImage}
+          ref={(img) => {
+            this.img = img;
+          }}
+        /> */}
     </div>
 
     );
