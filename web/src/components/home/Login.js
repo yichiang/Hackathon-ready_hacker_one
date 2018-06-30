f66e0043ee6b65eed68e185b6056bb39b3947e68
yichiang <yi_chiang@Outlook.com>
2018-06-30 14:02:11 -0700

    add css

--- web/src/components/home/Login.js
+++ web/src/components/home/Login.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import './../../App.css';
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Checkbox, Form, Segment } from 'semantic-ui-react'
+import homeStyle from './../../css/home.css';
 
 class Login extends Component {
   render() {
     return (
-      <Form>
-      <Form.Field>
-        <label>First Name</label>
-        <input placeholder='First Name' />
-      </Form.Field>
-      <Form.Field>
-        <label>Last Name</label>
-        <input placeholder='Last Name' />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label='I agree to the Terms and Conditions' />
-      </Form.Field>
-      <Button type='submit'>Submit</Button>
-    </Form>
+      <div>
+        <div className={homeStyle.pl_login_form}> Hi </div>
+      <Segment raised className={homeStyle.pl_login_form}>
+        <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder='First Name' />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder='Last Name' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+        </Form>
+
+      </Segment>
+    </div>
+
     );
   }
 }
