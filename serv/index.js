f46455052f233b919ad6ee09a5ad3daa9ff49928
yichiang <yi_chiang@outlook.com>
2018-06-30 19:35:45 -0700

    Remove slash needed

--- serv/index.js
+++ serv/index.js
@@ -3,7 +3,7 @@ var itemService = require('./service/item.js');
 var port = 8080;
 
 var handleRequest = function(request, response){
-    if(request.url.indexOf('api/item/') !== -1){
+    if(request.url.indexOf('api/item') !== -1){
       itemService.getAvailableItems(function(items){
         response.end(JSON.stringify(items));
       });
