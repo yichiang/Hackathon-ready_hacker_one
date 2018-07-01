a0095709178ea639c50255637573923d07df8169
yichiang <yi_chiang@Outlook.com>
2018-06-30 18:14:16 -0700

    fix page

--- web/src/components/menu/MainPage.js
+++ web/src/components/menu/MainPage.js
@@ -5,7 +5,7 @@ import './../../css/menu.css';
 import menuData_json from './../../data/menu.json';
 import PropTypes from 'prop-types';
 import MenuCard from './MenuCard';
-
+import Basket from '../order/Basket';
 import {
   Button,
   Checkbox,
@@ -18,66 +18,6 @@ import {
   Sidebar,
 } from 'semantic-ui-react'
 
-const HorizontalSidebar = ({ animation, direction, visible }) => (
-  <Sidebar as={Segment} animation={animation} direction={direction} visible={visible}>
-    <Grid textAlign='center'>
-      <Grid.Row columns={1}>
-        <Grid.Column>
-          <Header as='h3'>New Content Awaits</Header>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid columns={3} divided>
-        <Grid.Column>
-          <Image src='/images/wireframe/media-paragraph.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/wireframe/media-paragraph.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/wireframe/media-paragraph.png' />
-        </Grid.Column>
-      </Grid>
-    </Grid>
-  </Sidebar>
-)
-
-HorizontalSidebar.propTypes = {
-  animation: PropTypes.string,
-  direction: PropTypes.string,
-  visible: PropTypes.bool,
-}
-
-const VerticalSidebar = ({ animation, direction, visible }) => (
-  <Sidebar
-    as={Menu}
-    animation={animation}
-    direction={direction}
-    icon='labeled'
-    inverted
-    vertical
-    visible={visible}
-    width='thin'
-  >
-    <Menu.Item as='a'>
-      <Icon name='home' />
-      Home
-    </Menu.Item>
-    <Menu.Item as='a'>
-      <Icon name='gamepad' />
-      Games
-    </Menu.Item>
-    <Menu.Item as='a'>
-      <Icon name='camera' />
-      Channels
-    </Menu.Item>
-  </Sidebar>
-)
-
-VerticalSidebar.propTypes = {
-  animation: PropTypes.string,
-  direction: PropTypes.string,
-  visible: PropTypes.bool,
-}
 
 
 class MenuMain extends Component {
@@ -86,7 +26,10 @@ class MenuMain extends Component {
   direction: 'left',
   dimmed: false,
   visible: false,
-  menuData: menuData_json
+  menuData: menuData_json,
+  selecItems:[],
+  currentTotal: 10,
+
 }
 
 handleAnimationChange = animation => () =>
@@ -101,15 +44,31 @@ handleChangeOrder=(index, diff_count)=>{
   const menuData = this.state.menuData;
   if(menuData[index].order == null){
     menuData[index].order = 0;
+    // trigger sidebar after first time adjust item;
+    if(this.state.selecItems.length == 0)
+    {
+      this.setState({visible: true});
+    }
   }
-  if(menuData[index].order+=diff_count >=0){
-    menuData[index].order+=diff_count;    
+  var calcMenu = [];
+  if(menuData[index].order + diff_count >= 0){
+    menuData[index].order += diff_count;
   }
-  this.setState({menuData: menuData })
-
+  calcMenu = menuData.filter(x=> (x.order != null && x.order > 0))
+  this.setState({menuData: menuData, selecItems: calcMenu })
+  this.calcTotal(calcMenu);
+}
+calcTotal = (items) =>{
+  var total = 0;
+  for (var i = 0; i < items.length; i++) {
+    if(items[i].order != null){
+      total+=items[i].order * items[i].price;
+    }
+  }
+  this.setState({currentTotal: total})
 }
 render() {
-  const { animation, dimmed, direction, visible, menuData } = this.state
+  const { animation, dimmed, direction, visible, menuData, selecItems, currentTotal } = this.state
   const vertical = direction === 'bottom' || direction === 'top'
 
   return (
@@ -118,15 +77,26 @@ render() {
 
 
       <Sidebar.Pushable as={Segment}>
-        {vertical ? (
-          <HorizontalSidebar animation={animation} direction={direction} visible={visible} />
-        ) : null}
+
         {vertical ? null : (
-          <VerticalSidebar animation={animation} direction={direction} visible={visible} />
+          <Sidebar
+            className='menu_sidebar'
+            as={Menu}
+            animation={animation}
+            direction={direction}
+            icon='labeled'
+            inverted
+            vertical
+            visible={visible}
+            width='wide'
+          >
+
+            <Basket selecItems={selecItems} currentTotal={currentTotal}/>
+          </Sidebar>
         )}
 
         <Sidebar.Pusher dimmed={dimmed && visible}>
-          <Segment basic>
+          <Segment basic className={visible? 'minWidth':''}>
             <Header as='h2'>
               <Icon name='food'/>
               Order #20092
@@ -135,7 +105,7 @@ render() {
               {menuData.map((x, i)=>
                 {return (
                   <div>
-                    <MenuCard menu={x} handleChangeOrder={this.handleChangeOrder} itemIndex={i}></MenuCard>
+                    <MenuCard menu={x} handleChangeOrder={this.handleChangeOrder} itemIndex={i} selecItems={selecItems}></MenuCard>
                   </div>
                 )}
               )
